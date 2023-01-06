249d9ce0594aa2021c7b8ea261dee2c73c4f49ab
offsong <59704429+offsong@users.noreply.github.com>
2023-01-06 10:51:12 +0900

    Update cookie_kingdom.php

--- coupon/cookie_kingdom.php
+++ coupon/cookie_kingdom.php
@@ -2,11 +2,13 @@
 {
 	"name": "먹방일기 - 맛있게 먹으면 0칼로리",
 	"date" : "Pig Diary - Food record Diary",
-    "sub": "etc"
+        "sub1": "etc",
+        "sub2": "etc"
 },
 {
 	"name": "먹방일기 - 맛있게 먹으면 0칼로리",
 	"date" : "Pig Diary - Food record Diary",
-    "sub": "etc"
+        "sub1": "etc",
+        "sub2": "etc"
 }
 ]
