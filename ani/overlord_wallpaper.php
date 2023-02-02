c127fa681a86cc4adaaa84ce75cd18adef384216
offsong <59704429+offsong@users.noreply.github.com>
2023-02-02 22:15:07 +0900

    Update overlord_wallpaper.php

--- ani/overlord_wallpaper.php
+++ ani/overlord_wallpaper.php
@@ -1,9 +1,4 @@
 [
 
-"https://gaussian37.github.io/assets/img/vision/opencv/images_selection/2.jpg",
-"https://i.pinimg.com/originals/ba/d3/5c/bad35c732a66771fa4c9048a23c6ea4c.jpg",
-"https://i.pinimg.com/originals/6a/9f/76/6a9f76fe12820ab2aa678d5ac6292f5b.jpg",
-"https://i.pinimg.com/originals/7c/d6/7b/7cd67b98c624f5623efa08620f1b4654.jpg",
-"https://i.pinimg.com/originals/ce/c7/65/cec765f4e766906ebd22e4ef7c39208c.jpg",
-"https://i.pinimg.com/originals/4e/d2/1e/4ed21e320dae6f9d81c68b5cf6719577.jpg"
+"https://gaussian37.github.io/assets/img/vision/opencv/images_selection/2.jpg"
 ]
