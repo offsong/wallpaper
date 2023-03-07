566b7e3c6f555457919e52e8f355c1ecb22dc7c4
offsong <59704429+offsong@users.noreply.github.com>
2023-03-07 20:54:06 +0900

    Add files via upload

---
+++ ani/inuyasha/categories.php
@@ -0,0 +1,8 @@
+[
+  {
+    "name": "Inuyasha",
+    "preview1": "https://i.pinimg.com/originals/46/0a/ee/460aee30ee108c899b0e0928c3fcdc9e.jpg",
+    "preview2": "https://i.pinimg.com/originals/3b/3d/12/3b3d12d7f3061948ab54ca329686c600.jpg",
+    "preview3": "https://i.pinimg.com/originals/ac/8a/77/ac8a7726158301c90a67cec5abfd0460.jpg"
+  }
+]
